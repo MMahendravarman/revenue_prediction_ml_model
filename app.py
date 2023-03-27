8f38785ac6d81dd3abe8cc0122d85e367ca9f623
MMahendravarman <mahendravarman.m@gmail.com>
2023-03-27 10:19:43 +0530

    app changes

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
 import streamlit as st
 
+pickle_in = open("revenue_prediction.pkl","rb")
+model=pickle.load(pickle_in)
+
 def main():
     st.title('Restaurant Revenue Prediction')
+    if st.button("Predict"):
+        result=predict_revenue()
+    st.success('The output is {}'.format(result))
+
+def predict_revenue():
+    prediction=model.predict()
+    print(prediction)
+    return prediction    
 
 
 if __name__=='__main__':
