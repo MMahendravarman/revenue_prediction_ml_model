5702a55f6daccb56524070ab007d60e7f13de66f
MMahendravarman <mahendravarman.m@gmail.com>
2023-03-27 10:25:55 +0530

    app changes

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 import pickle
+import numpy
+import pandas as pd
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import linear_model
+from matplotlib import pyplot as plt
+from sklearn import tree
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 
 pickle_in = open("revenue_prediction.pkl","rb")
 model=pickle.load(pickle_in)
