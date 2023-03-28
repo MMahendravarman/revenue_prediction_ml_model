fb09f92042c0f43ddd7121f1952efcd267ff01ef
MMahendravarman <mahendravarman.m@gmail.com>
2023-03-28 23:18:16 +0530

    updated app file to include desc and sample values

--- app.py
+++ app.py
@@ -21,27 +21,32 @@ def main():
     <h2 style="color:white;text-align:center;">Streamlit Restaurant Revenue Prediction ML App </h2>
     </div>
     """
+
+    html_tmp= """
+    <p>Following inputs captures Demographic,real estate and commercial data.These fields are populated with sample values</p>
+    """
     st.markdown(html_temp,unsafe_allow_html=True)
     year = st.text_input("Year","Type Here")
-    Big_Cities = st.text_input("Big Citie","Type Here")
-    Other = st.text_input("Other","Type Here")
-    P1 = st.text_input("P1","Type Here")
-    P2 = st.text_input("P2","Type Here")
-    P4 = st.text_input("P4","Type Here")
-    P5 = st.text_input("P5","Type Here")
-    P6 = st.text_input("P6","Type Here")
-    P7 = st.text_input("P7","Type Here")
-    P11 = st.text_input("P11","Type Here")
-    P14 = st.text_input("P14","Type Here")
-    P15 = st.text_input("P15","Type Here")
-    P17 = st.text_input("P17","Type Here")
-    P19 = st.text_input("P19","Type Here")
-    P20 = st.text_input("P20","Type Here")
-    P21 = st.text_input("P21","Type Here")
-    P23 = st.text_input("P23","Type Here")
-    P24 = st.text_input("P24","Type Here")
-    P25 = st.text_input("P25","Type Here")
-    P28 = st.text_input("P28","Type Here")
+    Big_Cities = st.text_input("Big Cities","Enter 1 if Big City else 0")
+    Other = st.text_input("Other","Enter 1 for cities other than Big Cities else 0")
+    st.markdown(html_tmp,unsafe_allow_html=True)
+    P1 = st.text_input("P1",3)
+    P2 = st.text_input("P2",4)
+    P4 = st.text_input("P4",4)
+    P5 = st.text_input("P5",5)
+    P6 = st.text_input("P6",3)
+    P7 = st.text_input("P7",1)
+    P11 = st.text_input("P11",2)
+    P14 = st.text_input("P14",5)
+    P15 = st.text_input("P15",5)
+    P17 = st.text_input("P17",5)
+    P19 = st.text_input("P19",3)
+    P20 = st.text_input("P20",4)
+    P21 = st.text_input("P21",4)
+    P23 = st.text_input("P23",1)
+    P24 = st.text_input("P24",2)
+    P25 = st.text_input("P25",0)
+    P28 = st.text_input("P28",12.5)
     result=""
     if st.button("Predict"):
         result=predict_revenue(year,Big_Cities,Other,P1,P2,P4,P5,P6,P7,P11,P14,P15,P17,P19,P20,P21,P23,P24,P25,P28)
